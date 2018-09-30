62c3710e35a4d39f1ad8f7eab85b8c11a13bea65
bo <38799276@qq.com>
2018-09-30 16:02:29 +0800

    project commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <span class="tips"># 上拖翻页,下拖上一页</span>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style  lang="stylus">
+body, html
+  margin 0
+  padding 0
+  width 100%
+  height 100%
+  #app
+    width 100%
+    height 100%
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    .tips
+      position absolute
+      color #ddd
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-touch class="touch-content" @panstart="panstart" @panup="panup" @pandown="pandown" @panend="panend">
+    <div class="page">
+      <div class="img-wraper">
+        <img class="img" :src="currImg" />
+      </div>
+      </div>
+  </v-touch>
+</template>
+
+<script>
+import img0 from '@/assets/img0.jpg'
+import img1 from '@/assets/img1.jpg'
+import re from '@/assets/re.jpg'
+import re0 from '@/assets/re0.jpg'
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      actionStatus: 0, // 0 normal, 1 panup, 2 pandown
+      currImg: '',
+      listIdx: 0,
+      reFlag: false,
+      normalList: [img0, img1],
+      reList: [re, re0]
+    }
+  },
+  created() {
+    this.currImg = this.imgList[this.listIdx]
+  },
+  watch: {
+    listIdx() {
+      if (!this.reFlag && this.listIdx > 1) { // 过完第一个normalList的图片后
+        this.reFlag = true
+        this.listIdx = 0
+      }
+      this.currImg = this.imgList[this.listIdx]
+    }
+  },
+  computed: {
+    imgList() {
+      return this.reFlag ? this.reList : [...this.normalList, ...this.reList]
+    },
+  },
+  methods: {
+    panstart(params) {
+    },
+    panup(params) {
+      this.actionStatus = 1
+    },
+    pandown(params) {
+      this.actionStatus = 2
+    },
+    panend() {
+      this.actionStatus == 1 ? this.next() : this.prev()
+      this.actionStatus = 0
+    },
+    prev() {
+      let idx = this.listIdx - 1
+      if(this.reFlag){
+        this.listIdx = idx < 0 ? this.imgList.length - 1 : idx
+      }else{
+        this.listIdx = idx < 0 ? 0 : idx
+      }
+      console.log(this.listIdx);
+    },
+    next() {
+      let idx = this.listIdx + 1
+      this.listIdx = idx > this.imgList.length - 1 ? 0 : idx
+      console.log(this.listIdx);
+    },
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.touch-content
+  width 100%
+  height 100%
+  .page
+    width 100%
+    height 100%
+    display flex
+    justify-content center
+    align-items center
+    .img
+      display inline-block
+      width 100%
+</style>
